3b9af05cabbf62ccef7742dd3402cfccfaae19b2
jpena146 <goodjp23@yahoo.com>
2022-02-27 15:09:55 -0600

    local changes

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,18 +1,18 @@
 <template>
-<div class = "bg-yellow-800 text-yellow-200">
+<div class = "bg-purple-800 text-purple-200">
     <div class = "container mx-auto flex items-center justify-between ">
-        <h1 class="tracking-tighter text-3xl font-thin">Basic<span class="font-normal">Router</span>
+        <h1 class="tracking-tighter text-3xl font-thin">Vue<span class="font-normal">FakeAuth</span>
         </h1>
 
         <nav>
             <ul class = "flex space-x-4">
-                <router-link to ="/"><li class="py-8 px-4 hover:cursor-pointer hover:bg-yellow-500 hover:text-yellow-800">Home
+                <router-link to ="/"><li class="py-8 px-4 hover:cursor-pointer hover:bg-purple-500 hover:text-purple-800">Home
                 </li> </router-link>
                 <router-link to ="/about">
-                <li class="py-8 px-4 hover:cursor-pointer hover:bg-yellow-500 hover:text-yellow-800">About
+                <li class="py-8 px-4 hover:cursor-pointer hover:bg-purple-500 hover:text-purple-800">About
                 </li></router-link>
                 <router-link to ="/hello">
-                <li class="py-8 px-4 hover:cursor-pointer hover:bg-yellow-500 hover:text-yellow-800">Hello</li></router-link>
+                <li class="py-8 px-4 hover:cursor-pointer hover:bg-purple-500 hover:text-purple-800">Hello</li></router-link>
                 
             </ul>
         </nav>
